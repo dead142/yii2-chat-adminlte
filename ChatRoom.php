6411e3b702993057cb9202b70eca6914b39f4bcf
Andrei V. Berezhkov <dead142@mail.ru>
2017-12-28 23:59:57 +0300

    Update ChatRoom.php

--- ChatRoom.php
+++ ChatRoom.php
@@ -46,8 +46,8 @@ class ChatRoom extends Widget {
         }
 
         $this->model->userField = $this->userField;
-        Yii::$app->assetManager->publish("@vendor/sintret/yii2-chat-adminlte/assets/img/loadingAnimation.gif");
-        $this->loadingImage = Yii::$app->assetManager->getPublishedUrl("@vendor/sintret/yii2-chat-adminlte/assets/img/loadingAnimation.gif");
+        Yii::$app->assetManager->publish("@vendor/sintret-2017/yii2-chat-adminlte/assets/img/loadingAnimation.gif");
+        $this->loadingImage = Yii::$app->assetManager->getPublishedUrl("@vendor/sintret-2017/yii2-chat-adminlte/assets/img/loadingAnimation.gif");
 
         parent::init();
     }
